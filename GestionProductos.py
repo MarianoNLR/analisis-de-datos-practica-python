548d71063fc848017911dee58aba3e6dac9d6153
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-27 16:40:28 -0300

    leer producto

--- GestionProductos.py
+++ GestionProductos.py
@@ -1,4 +1,7 @@
 import json
+import ProductoAlimenticio
+import ProductoElectronico
+import Producto
 
 class GestionProductos:
     def __init__(self, archivo):
@@ -23,3 +26,33 @@ class GestionProductos:
             print(f"Error al intentar guardar los datos en el archivo: {error}")
         except Exception as error:
             print(f"Error Inesperado: {error}")
+    
+    def leer_producto(self, codigo_producto):
+        try:
+            datos = self.leer_datos()
+            mensaje_consola = ""
+            if codigo_producto in datos:
+                producto_datos = datos[codigo_producto]
+                
+                if 'fecha_vencimiento' in producto_datos:
+                    producto = ProductoAlimenticio(**producto_datos)
+                    mensaje_consola += """==== Producto Alimenticio ====\n"""
+                else:
+                    producto = ProductoElectronico(**producto_datos)
+                    mensaje_consola += """==== Producto Electronico ====\n"""
+                mensaje_consola += f"{producto}"
+                print(mensaje_consola)
+            else:
+                print(f"No se ha encontrado un producto con el codigo: {codigo_producto}.")
+                
+        except Exception as error:
+            print(f"Error al leer el producto: {error}")
+            
+    def agregar_producto(self, producto):
+        pass
+    
+    def eliminar_producto(self):
+        pass
+    
+    def actualizar_producto(self, codigo_producto):
+        pass
