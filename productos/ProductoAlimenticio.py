2bb1456646814efb9e13f6e1ddd6704d5f075ee3
Mariano Lotero Rolon <marianonehuenlr@gmail.com>
2024-07-30 18:22:29 -0300

    validaciones fecha de vencimiento

--- productos/ProductoAlimenticio.py
+++ productos/ProductoAlimenticio.py
@@ -1,9 +1,10 @@
 from productos.Producto import Producto
+import datetime
 
 class ProductoAlimenticio(Producto):
     def __init__(self, codigo_producto, nombre, precio, stock, marca, categoria, fecha_vencimiento, es_libre_gluten):
         super().__init__(codigo_producto, nombre, precio, stock, marca, categoria)
-        self._fecha_vencimiento = fecha_vencimiento
+        self._fecha_vencimiento = self.validar_fecha_vencimiento(fecha_vencimiento)
         self._es_libre_gluten = es_libre_gluten
         
     @property
@@ -13,6 +14,19 @@ class ProductoAlimenticio(Producto):
     @property
     def es_libre_gluten(self):
         return self._es_libre_gluten
+
+    @fecha_vencimiento.setter
+    def fecha_vencimiento(self, nueva_fecha):
+        self._fecha_vencimiento = self.validar_fecha_vencimiento(nueva_fecha)
+    
+    def validar_fecha_vencimiento(self, fecha):
+        try:
+            objeto_fecha = datetime.datetime.strptime(fecha, "%d/%m/%Y").date()
+            if objeto_fecha <= datetime.datetime.now().date():
+                raise ValueError(f"La fecha de vencimiento debe ser mayor a la fecha actual!")
+            return fecha
+        except ValueError as error:
+            raise ValueError(f"Error al cargar la fecha de vencimiento: La fecha se debe ingresar con el formato DD/MM/YYYY")
     
     def to_dict(self):
         data = super().to_dict()
